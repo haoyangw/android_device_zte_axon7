faa82dd6ef00dae61bcbea06732c0545c5c5a30f
NFound <NFound@yandex.ru>
2018-04-04 16:53:51 +0500

    axon7: Update thermal-engine to O

--- device.mk
+++ device.mk
@@ -390,6 +390,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
